9834b4e6e0f00f2a1a5c0b003529ba025356ec51
jtustanic <josipa.tustanic@gmail.com>
2023-05-11 18:07:31 +0200

    Header.js component added, added Sidebar component in App.js

--- src/App.js
+++ src/App.js
@@ -2,26 +2,40 @@ import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Chat from "./components/Chat";
 import Login from "./components/Login";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 
 class App extends React.Component {
 
-constructor(props){
-  super(props);
-  this.state={username:""};
-}
+  constructor(props) {
+    super(props);
+    this.state = { username: "" };
+  }
 
-handleLogin = (username) => {
-this.setState({username})
+  handleLogin = (username) => {
+    this.setState({ username })
 
-}
+  }
+
+  toggleSidebar = () =>{
+    this.sidebar.ToggleSidebar();
+  }
 
   render() {
     return (
- <Routes>
-<Route path="/" element={this.state.username ? <Chat username={this.state.username}/> : <Navigate to="/login" />} />
-<Route path="/login" element={<Login onLogin={this.handleLogin} />} />
- </Routes>
- )
+
+
+      <div>
+        <Header toggleSidebar={this.toggleSidebar} username={this.state.username} />
+        <Sidebar ref={(reference) => this.sidebar = reference} />
+
+
+        <Routes>
+          <Route path="/" element={this.state.username ? <Chat username={this.state.username} /> : <Navigate to="/login" />} />
+          <Route path="/login" element={<Login onLogin={this.handleLogin} />} />
+        </Routes>
+      </div>
+    )
   }
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+const Header = (props) => {
+    const isLoggedIn = !!props.username; //!! osiguravaju tocnost podataka koje cemo primati
+    return(
+        <> 
+        {isLoggedIn && 
+         <div className="App-header">
+         <button className='sidebar-btn' onClick={props.toggleSidebar}>Sidebar</button>
+         <h1>{props.username}'s Chat Room</h1>
+         {/* Prazan div služi za flex pozicioniranje elemenata */}
+         <div></div>
+       </div>
+       }
+       
+       {!isLoggedIn &&
+        <div className="App-header">
+       <div></div>
+        <h1>Chat App</h1>
+        {/* Prazan div služi za flex pozicioniranje elemenata */}
+        <div></div>
+      </div>
+      }
+
+      </>
+    )
+}
+
+export default Header;
